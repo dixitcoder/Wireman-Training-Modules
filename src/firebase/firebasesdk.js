f2de6f91e5d5e3c554c18e9f89b755d7702efa03
Dixit Patel <134264323+dixitcoder@users.noreply.github.com>
2025-01-09 19:16:37 +0530

    Update firebasesdk.js

--- src/firebase/firebasesdk.js
+++ src/firebase/firebasesdk.js
@@ -5,13 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCEnCMOXfW0b7NyT1eHayPpTqNjA-y45pE",
-    authDomain: "wireman-9e4e6.firebaseapp.com",
-    projectId: "wireman-9e4e6",
-    storageBucket: "wireman-9e4e6.firebasestorage.app",
-    messagingSenderId: "590297181331",
-    appId: "1:590297181331:web:67a293d09134907acbf166",
-    measurementId: "G-YG3H62PDNL"
+   // your api keya using now 
 };
 
 // Initialize Firebase
